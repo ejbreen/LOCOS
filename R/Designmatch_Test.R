a7ed6319a9191b055c07b8b736f0e615b490f7cf
ejbreen <ejbreen@umich.edu>
2018-01-30 11:24:09 -0500

    reconnecting to git

---
+++ R/Designmatch_Test.R
@@ -0,0 +1,46 @@
+# Required packages:    Rglpk
+#                       designmatch
+#                       gurobi      'C:/gurobi751/win64/R/gurobi_7.5-1.zip'
+
+install.packages('C:/gurobi752/win64/R/gurobi_7.5-2.zip', repos = NULL)
+
+library(gurobi)
+library(Rglpk)
+library(designmatch)
+
+data(lalonde)
+attach(lalonde)
+quantiles = function(covar, n_q) {
+    p_q = seq(0, 1, 1 / n_q)
+	val_q = quantile(covar, probs = p_q, na.rm = TRUE)
+	covar_out = rep(NA, length(covar))
+    for (i in 1:n_q) {
+        if (i == 1) { covar_out[covar < val_q[i + 1]] = i }
+        if (i > 1 & i < n_q) { covar_out[covar >= val_q[i] & covar < val_q[i + 1]] = i }
+        if (i == n_q) { covar_out[covar >= val_q[i] & covar < val_q[i + 1]] = i}
+	}
+	covar_out
+}
+
+age_5 = quantiles(age, 5)
+education_5 = quantiles(education, 5)
+re74_5 = quantiles(re74, 5)
+re75_5 = quantiles(re75, 5)
+
+# Treatment indicator
+t_ind = treatment
+
+# Fine Balance 
+fine_covs = cbind(black, hispanic, married, nodegree, age_5, education_5, re74_5, re75_5)
+fine = list(covs = fine_covs)
+
+# Solver options
+t_max = 60 * 5
+solver = 'gurobi'
+solver = list(name = solver, t_max = t_max, approximate = 0, round_cplex = 0, trace = 0)
+
+# Match it
+out_1 = cardmatch(t_ind, fine = fine, solver = solver)
+
+
+
---
+++ R/gurobi_test.R
@@ -0,0 +1,16 @@
+install.packages('C:/gurobi752/win64/R/gurobi_7.5-2.zip', repos = NULL)
+library(gurobi)
+
+model <- list()
+
+model$A <- matrix(c(1, 2, 3, 1, 1, 0), nrow = 2, ncol = 3, byrow = T)
+model$obj <- c(1, 1, 2)
+model$modelsense <- 'max'
+model$rhs <- c(4, 1)
+model$sense <- c('<=', '>=')
+model$vtype <- 'B'
+
+params <- list(OutputFlag = 0)
+
+result <- gurobi(model, params)
+
