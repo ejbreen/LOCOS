7b8ae1f982779e8379a07a2a6218b2a216a87050
Evan <33337724+ejbreen@users.noreply.github.com>
2018-02-13 23:43:08 -0500

    Update setup_flux.R

--- R/setup_flux.R
+++ R/setup_flux.R
@@ -1,6 +1,8 @@
 # required for gurobi
 install.packages('slam', repos = 'https://cloud.r-project.org/') # required for gurobi
 library(slam)
+
+# the location of gurobi on flux
 install.packages('/sw/arc/centos7/gurobi/gurobi751/linux64/R/gurobi_7.5-1_R_x86_64-pc-linux-gnu.tar.gz', repos = NULL)
 library(gurobi)
 
